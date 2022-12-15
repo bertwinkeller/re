022adad68a0c63f195aa53a1ca65fdd55f005a59
bertwinkeller <bertwinkeller@gmail.com>
2022-12-14 17:10:33 -0800

    hide .env file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Link from "next/link"
-import Image from "next/image"
+import Image from "next/image" 
 import { Flex, Box, Text, Button } from "@chakra-ui/react"
+import Property from '../components/Property';
+
+
+import { baseUrl, fetchApi } from "../utils/fetchApi"
 
 export const Banner = ({
   purpose,
@@ -28,38 +32,60 @@ export const Banner = ({
         <br />
         {desc2}
       </Text>
-      <Button fontSize='xl' bg="blue.300" color="white">
+      <Button fontSize="xl">
         <Link href={linkName}>{buttonText}</Link>
       </Button>
-      
     </Box>
   </Flex>
 )
-export default function Home() {
+
+export default function Home({ propertiesForSale, propertiesForRent }) {
+  console.log(propertiesForRent, propertiesForSale)
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <Box>
       <Banner
-      purpose='RENT A HOME'
-      title1='Rental Homes for'
-      title2='Everyone'
-      desc1=' Explore from Apartments, builder floors, villas'
-      desc2='and more'
-      buttonText='Explore Renting'
-      linkName='/search?purpose=for-rent'
-      imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
-    />
-  
-    <Banner
-      purpose='BUY A HOME'
-      title1=' Find, Buy & Own Your'
-      title2='Dream Home'
-      desc1=' Explore from Apartments, land, builder floors,'
-      desc2=' villas and more'
-      buttonText='Explore Buying'
-      linkName='/search?purpose=for-sale'
-      imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
-    />
-    </div>
+        purpose="RENT A HOME"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1=" Explore from Apartments, builder floors, villas"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      <Flex flexWrap="wrap">
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
+      <Banner
+        purpose="BUY A HOME"
+        title1=" Find, Buy & Own Your"
+        title2="Dream Home"
+        desc1=" Explore from Apartments, land, builder floors,"
+        desc2=" villas and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      />
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
+    </Box>
   )
+} 
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+  )
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+  )
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  }
 }
